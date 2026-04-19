b2be5735de45d561f1fa5458d10a16303f756ecf
Hithesh <hitheshsjecmca@gmail.com>
2026-04-19 12:47:31 +0530

    date time chnages

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import datetime
 
 # Load face detection model
 face_cascade = cv2.CascadeClassifier(
@@ -9,14 +10,20 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
 
+
+    time_text = datetime.datetime.now().strftime("%H:%M:%S")
+    cv2.putText(frame, time_text, (10, 100),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 8)
-        print(len(faces))
+        cv2.putText(frame, f"Faces: {len(faces)}", (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow("Face Detection", frame)
 
